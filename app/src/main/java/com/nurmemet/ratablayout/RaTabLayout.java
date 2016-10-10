5d50559eb096e558489cc825336930098336c2eb
nurmemet <2335515050@email.com>
2016-10-10 10:04:53 +0800

    添加了指示器

--- app/src/main/java/com/nurmemet/ratablayout/RaTabLayout.java
+++ app/src/main/java/com/nurmemet/ratablayout/RaTabLayout.java
@@ -6,10 +6,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
 import android.graphics.RectF;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -24,6 +23,11 @@ import android.widget.TextView;
  */
 public class RaTabLayout extends LinearLayout {
 
+    private enum DecorateMode {
+        MODE_1, MODE_2
+    }
+
+    private DecorateMode mAnimMode = DecorateMode.MODE_2;
     private PagerAdapter adapter;
     private ViewPager viewPager;
     private BindView bindView;
@@ -34,7 +38,7 @@ public class RaTabLayout extends LinearLayout {
 
     private int mX1;
     private int mX2;
-    private boolean isDecorateMode = false;
+    private boolean isDecorateMode = true;
     private int mLineHeight = 2;
     private int mArcRadius = 15;
     private int mInset = 35;
@@ -43,9 +47,13 @@ public class RaTabLayout extends LinearLayout {
     private Paint mPaint;
     private boolean withAnim = false;
 
+    private TriangleDecorate mTriangleDecorate;
+
     public void setDecorateMode(boolean isDecorateMode) {
         this.isDecorateMode = isDecorateMode;
     }
+
+
 
     public RaTabLayout(Context context) {
         super(context);
@@ -63,6 +71,8 @@ public class RaTabLayout extends LinearLayout {
     private void init() {
         this.setOrientation(HORIZONTAL);
         setWillNotDraw(false);
+        mTriangleDecorate = new TriangleDecorate();
+
         //setBackgroundColor(ContextCompat.getColor(getContext(), R.color.main_bg_gray));
     }
 
@@ -206,36 +216,44 @@ public class RaTabLayout extends LinearLayout {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (isDecorateMode) {
-            if (mPaint == null) {
-                mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-                mPaint.setColor(Color.RED);
-                mPaint.setStrokeWidth(mLineHeight);
-                mPaint.setStyle(Paint.Style.STROKE);
-                mPath = new Path();
+            if (mAnimMode == DecorateMode.MODE_1) {
+                if (mPaint == null) {
+                    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                    mPaint.setColor(Color.RED);
+                    mPaint.setStrokeWidth(mLineHeight);
+                    mPaint.setStyle(Paint.Style.STROKE);
+                    mPath = new Path();
 
+                }
+
+                mPath.reset();
+                mPath.moveTo(0, getHeight() - mLineHeight);
+                mPath.lineTo(mX1 - mArcRadius, getHeight() - mLineHeight);
+                mPath.moveTo(mX1 - mArcRadius, getHeight() - mLineHeight);
+
+                mPath.addArc(new RectF(mX1 - 2 * mArcRadius, getHeight() - mLineHeight - 2 * mArcRadius, mX1, getHeight() - mLineHeight), 0, 90);
+                mPath.moveTo(mX1, getHeight() - mLineHeight - mArcRadius);
+                mPath.lineTo(mX1, mLineHeight + mArcRadius + mInset);
+                mPath.addArc(new RectF(mX1, mLineHeight + mInset, mX1 + 2 * mArcRadius, mLineHeight + 2 * mArcRadius + mInset), 180, 90);
+
+                mPath.moveTo(mX1 + mArcRadius, mLineHeight + mInset);
+                mPath.lineTo(mX2 - mArcRadius, mLineHeight + mInset);
+                mPath.addArc(new RectF(mX2 - 2 * mArcRadius, mLineHeight + mInset, mX2, mLineHeight + 2 * mArcRadius + mInset), 270, 90);
+
+                mPath.moveTo(mX2, mLineHeight + mArcRadius + mInset);
+                mPath.lineTo(mX2, getHeight() - mLineHeight - mArcRadius);
+                mPath.addArc(new RectF(mX2, getHeight() - 2 * mArcRadius - mLineHeight, mX2 + 2 * mArcRadius, getHeight() - mLineHeight), 90, 90);
+
+                mPath.moveTo(mX2 + mArcRadius, getHeight() - mLineHeight);
+                mPath.lineTo(getWidth() - mLineHeight, getHeight() - mLineHeight);
+
+                canvas.drawPath(mPath, mPaint);
+            } else if (mAnimMode == DecorateMode.MODE_2) {
+                final float itemWidth = (1F) / getChildCount() * getMeasuredWidth();
+                final float x = itemWidth / 2 + mCurrentPosition * itemWidth;
+                final float y = getMeasuredHeight();
+                mTriangleDecorate.draw(canvas, x, y);
             }
-            mPath.reset();
-            mPath.moveTo(0, getHeight() - mLineHeight);
-            mPath.lineTo(mX1 - mArcRadius, getHeight() - mLineHeight);
-            mPath.moveTo(mX1 - mArcRadius, getHeight() - mLineHeight);
-
-            mPath.addArc(new RectF(mX1 - 2 * mArcRadius, getHeight() - mLineHeight - 2 * mArcRadius, mX1, getHeight() - mLineHeight), 0, 90);
-            mPath.moveTo(mX1, getHeight() - mLineHeight - mArcRadius);
-            mPath.lineTo(mX1, mLineHeight + mArcRadius + mInset);
-            mPath.addArc(new RectF(mX1, mLineHeight + mInset, mX1 + 2 * mArcRadius, mLineHeight + 2 * mArcRadius + mInset), 180, 90);
-
-            mPath.moveTo(mX1 + mArcRadius, mLineHeight + mInset);
-            mPath.lineTo(mX2 - mArcRadius, mLineHeight + mInset);
-            mPath.addArc(new RectF(mX2 - 2 * mArcRadius, mLineHeight + mInset, mX2, mLineHeight + 2 * mArcRadius + mInset), 270, 90);
-
-            mPath.moveTo(mX2, mLineHeight + mArcRadius + mInset);
-            mPath.lineTo(mX2, getHeight() - mLineHeight - mArcRadius);
-            mPath.addArc(new RectF(mX2, getHeight() - 2 * mArcRadius - mLineHeight, mX2 + 2 * mArcRadius, getHeight() - mLineHeight), 90, 90);
-
-            mPath.moveTo(mX2 + mArcRadius, getHeight() - mLineHeight);
-            mPath.lineTo(getWidth() - mLineHeight, getHeight() - mLineHeight);
-
-            canvas.drawPath(mPath, mPaint);
 
 
         }
@@ -252,5 +270,62 @@ public class RaTabLayout extends LinearLayout {
                 mX2 = view.getWidth() + mX1 - 2 * mInset;
             }
         }
+    }
+
+    public void setTriangleRadius(float radius) {
+        mTriangleDecorate.setTriangleRadius(radius);
+    }
+
+    public void setTriangleColor(int color) {
+        mTriangleDecorate.setTriangleColor(color);
+    }
+
+
+    private static class TriangleDecorate {
+
+        private Paint mPaint;
+        private int mColor = 0xFFFFA500;
+        private float mRadius = 20;
+        private Path mPath;
+        private PointF mFirstP = new PointF(), mSecondP = new PointF(), mThirdP = new PointF();
+
+        void init() {
+            mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            mPaint.setColor(mColor);
+            mPath = new Path();
+
+        }
+
+        public TriangleDecorate() {
+            init();
+        }
+
+        void draw(Canvas canvas, float x, float y) {
+
+            mPath.reset();
+
+            mFirstP.x = x - mRadius;
+            mFirstP.y = y;
+            mSecondP.x = x;
+            mSecondP.y = y - (float) (mRadius * Math.sqrt(3));
+            mThirdP.x = x + mRadius;
+            mThirdP.y = y;
+
+            mPath.moveTo(mFirstP.x, mFirstP.y);
+            mPath.lineTo(mSecondP.x, mSecondP.y);
+            mPath.lineTo(mThirdP.x, mThirdP.y);
+
+            canvas.drawPath(mPath, mPaint);
+
+        }
+
+        void setTriangleRadius(float radius) {
+            mRadius = radius;
+        }
+
+        void setTriangleColor(int color) {
+            mColor = color;
+        }
+
     }
 }
